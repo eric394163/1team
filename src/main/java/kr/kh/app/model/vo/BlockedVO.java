fe416f9fbb4977b3c46856810441817195c52e94
KIM SUNG HYUN <eric394163@gmail.com>
2024-03-06 12:58:58 +0900

    ksh_model수정

---
+++ src/main/java/kr/kh/app/model/vo/BlockedVO.java
@@ -0,0 +1,12 @@
+package kr.kh.app.model.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class BlockedVO {
+    private int blocked_id;
+    private String blocking_user_id;
+    private String blocked_user_id;
+}
