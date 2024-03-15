daf30f1f1052d20f7de7ac01932adad524fe9cdd
KIM SUNG HYUN <eric394163@gmail.com>
2024-03-15 18:50:57 +0900

    11

--- src/main/java/kr/kh/app/controller/PostInsertServlet.java
+++ src/main/java/kr/kh/app/controller/PostInsertServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
-import kr.kh.app.model.dto.LoginDTO;
+import kr.kh.app.model.vo.AttachVO;
 import kr.kh.app.model.vo.BoardVO;
 import kr.kh.app.model.vo.PostVO;
 import kr.kh.app.model.vo.UserVO;
@@ -31,25 +31,18 @@ public class PostInsertServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		MainServlet.commonAsideInfo(request);
-		
-			// 게시판 전체 가져옴
-			ArrayList<BoardVO> list = postService.getBoardList();
-			request.setAttribute("list", list);
-			request.getRequestDispatcher("/WEB-INF/views/post/insert.jsp").forward(request, response);
+
+		// 게시판 전체 가져옴
+		ArrayList<BoardVO> list = postService.getBoardList();
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/WEB-INF/views/post/insert.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		UserVO user = (UserVO)session.getAttribute("user") ;
-		
-		
-		if (user == null) {
-			response.sendRedirect(request.getContextPath() + "/post/list");
-			return;
-		}
-		
+		UserVO user = (UserVO) session.getAttribute("user");
+
 		int post_board_num = Integer.parseInt(request.getParameter("board"));
 		String post_title = request.getParameter("title");
 		String post_content = request.getParameter("content");
@@ -57,8 +50,17 @@ public class PostInsertServlet extends HttpServlet {
 		Date post_date = new Date();
 
 		PostVO post = new PostVO(post_board_num, post_title, post_content, post_user_id, post_date);
-		
-		if(postService.insertPost(post)) {
+
+		// 첨부파일 가져오기
+		ArrayList<Part> partList = (ArrayList<Part>) request.getParts();
+
+		// 주소 가져오기
+		String link = request.getParameter("youtube");
+		AttachVO attach = new AttachVO(post_board_num, link);
+
+		// 서비스에게 게시글 객체를 주면서 등록하라고 시킴
+
+		if (postService.insertPost(post, partList, attach)) {
 			response.sendRedirect(request.getContextPath() + "/post/list");
 		} else {
 			response.sendRedirect(request.getContextPath() + "/post/insert");
