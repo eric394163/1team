68c5c34e100d0f34c869b7c4c772c4a2a58da9e6
KIM SUNG HYUN <eric394163@gmail.com>
2024-03-15 18:15:44 +0900

    1

--- src/main/java/kr/kh/app/service/PostService.java
+++ src/main/java/kr/kh/app/service/PostService.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 
 import kr.kh.app.model.vo.BoardVO;
 import kr.kh.app.model.vo.PostVO;
-import kr.kh.app.pagination.Criteria;
+
 
 public interface PostService {
 
 	ArrayList<BoardVO> getBoardList();
 
 	boolean insertPost(PostVO post);
+
+<<<<<<< HEAD
+=======
+	ArrayList<PostVO> getTotalPostList();	// 전체 게시글 조회
+>>>>>>> b11be37048b754907451b8d3ea8a9165e0cccd9d
 	
 }
